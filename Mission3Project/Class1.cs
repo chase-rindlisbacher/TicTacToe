5f31ac8304932e2e568fafc838961d0eb831de5b
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-01-24 15:41:42 -0700

    finished game

---
+++ Mission3Project/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission3Project
+{
+    class Support
+    {
+        string[] normal = { "1", "2", "3", "4", "5", "6", "7", "8","9" };
+        public void PrintCurrentBoard(string[] positions)
+        {
+            Console.WriteLine("    |     |    ");
+            Console.WriteLine(" " + positions[0] + "  |  " + positions[1] + "  |  " + positions[2] + " ");
+            Console.WriteLine("____|_____|____");
+            Console.WriteLine("    |     |    ");
+            Console.WriteLine(" " + positions[3] + "  |  " + positions[4] + "  |  " + positions[5] + " ");
+            Console.WriteLine("____|_____|____");
+            Console.WriteLine("    |     |    ");
+            Console.WriteLine(" " + positions[6] + "  |  " + positions[7] + "  |  " + positions[8] + " ");
+            Console.WriteLine("    |     |    ");
+        }
+        public bool Winner(string[] positions)
+        {
+            int catGame = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (positions[i] == normal[i])
+                {
+                    catGame++;
+                }
+            }
+            //top horizontal
+            if (positions[0] == positions[1] && positions[1] == positions[2])
+            {
+                Console.WriteLine(positions[0] + " wins!");
+                return true;
+            }
+            //diagonal sloping down to the right
+            else if (positions[0] == positions[4] && positions[4] == positions[8])
+            {
+                Console.WriteLine(positions[0] + " wins!");
+                return true;
+            }
+            // left vertical
+            else if (positions[0] == positions[3] && positions[3] == positions[6])
+            {
+                Console.WriteLine(positions[0] + " wins!");
+                return true;
+            }
+            // middle horizontal
+            else if (positions[3] == positions[4] && positions[4] == positions[5])
+            {
+                Console.WriteLine(positions[3] + " wins!");
+                return true;
+            }
+            // bottom horizontal
+            else if (positions[6] == positions[7] && positions[7] == positions[8])
+            {
+                Console.WriteLine(positions[6] + " wins!");
+                return true;
+            }
+            // middle vertical
+            else if (positions[1] == positions[4] && positions[4] == positions[7])
+            {
+                Console.WriteLine(positions[1] + " wins!");
+                return true;
+            }
+            // right vertical
+            else if (positions[2] == positions[5] && positions[5] == positions[8])
+            {
+                Console.WriteLine(positions[2] + " wins!");
+                return true;
+            }
+            // diagonal sloping down to the left
+            else if (positions[2] == positions[4] && positions[4] == positions[6])
+            {
+                Console.WriteLine(positions[2] + " wins!");
+                return true;
+            }
+            else if (catGame == 0)
+            {
+                Console.WriteLine("Cat game!");
+                return true;
+            }
+            // return no winner
+            else
+            {
+                Console.WriteLine("No winner");
+                return false;
+            }
+        }
+    }
+}
+
--- Mission3Project/Program.cs
+++ Mission3Project/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
-
+// Chase Rindlisbacher, Izzy Sampson, James Lear, Eddy Rodriguez
+// Mission 3 Assignment, Tic-Tac-Toe game
 namespace Mission3Project
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Support su = new Support();
+            // Welcome players to the game
+            Console.WriteLine("Welcome! I hope you're ready to play some TicTacToe!");
+            // create board array, it will be printed before each move, position placeholders will be replaced
+            // as players place their X's and O's
+            string[] board = new string[] {"1","2","3","4","5","6","7","8","9"};
+            // print board method
+            su.PrintCurrentBoard(board);
+
+            // Set boolean for if the game is over
+            Boolean Gameover = false;
+            // ask players for choice and update the array
+            while (Gameover == false)
+            {
+                // Set boolean for whether you should be asking a player to place their guess until they enter a valid position
+                Boolean keepgoing = true;
+                while (keepgoing == true)
+                {
+                    Console.WriteLine("Player 1, where would you like to place your next X");
+                    // grab their input then convert to an int
+                    string playerGuess1 = Console.ReadLine();
+                    int Guess1 = Convert.ToInt32(playerGuess1);
+                    // begin if statement to check if the place they're guessing has already been taken then update array
+                    if (Guess1 > 0 && Guess1 < 10 && board[(Guess1 - 1)] != "O" && board[(Guess1 - 1)] != "X")
+                    {
+                        board[(Guess1 - 1)] = "X";
+                        keepgoing = false;
+                    }
+                    // force them to pick a spot that hasn't already been selected
+                    else
+                    {
+                        Console.WriteLine("Try again, but this time in a valid position");
+                    }
+                }
+                // printboard method
+                su.PrintCurrentBoard(board);
+                // checkwin method
+                Gameover = su.Winner(board);
+                // reset boolean to true for while loop
+                if (Gameover == true) 
+                {
+                    keepgoing = false;
+                }
+                else
+                {
+                    keepgoing = true;
+                }
+                // same thing as you did with player 1
+                while (keepgoing == true)
+                {
+
+                    Console.WriteLine("Player 2, where would you like to place your next O");
+                    string playerGuess2 = Console.ReadLine();
+                    int Guess2 = Convert.ToInt32(playerGuess2);
+                    if (Guess2 > 0 && Guess2 < 10 && board[(Guess2 - 1)] != "O" && board[(Guess2 - 1)] != "X")
+                    {
+                        board[(Guess2 - 1)] = "O";
+                        keepgoing = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again, but this time in a valid position");
+                    }
+                }
+                // printboard method
+                su.PrintCurrentBoard(board);
+                // checkwin method
+                Gameover = su.Winner(board);
+            }
         }
     }
 }
